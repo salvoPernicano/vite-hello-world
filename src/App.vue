09873d553d8b9223038b9710605407db00e7fc40
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-06 18:43:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+export default{
+  components:{
+    AppTitle,
+    AppHero
+  }
+}
+</script>
+
+
+<template>
+    <AppTitle/>
+   <AppHero/>
+</template>
+
+
+<style scoped>
+  body{
+    text-align: center;
+  }
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+  name : "AppHero"
+}
+</script>
+
+<template>
+<img src="https://static.wikia.nocookie.net/7ce76513-f254-44f1-ac22-0163b3dcb6f8/scale-to-width/755" alt="">
+</template>
+
+<style scoped>
+img{
+    width: 350px;
+    height: 350px;
+    display: block;
+    margin: 0 auto;
+}
+</style>
